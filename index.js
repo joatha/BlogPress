d68b47517356d0dc58dc8dd0406c7b2e769259b7
Joathã Andrade <joathaandrade2@gmail.com>
2021-07-11 10:18:15 -0300

    Atualizando

--- index.js
+++ index.js
@@ -38,7 +38,10 @@ app.get("/", (req, res) =>{
             ['id', 'DESC']
         ]
     }).then(articles =>{
-        res.render("index", {articles: articles})
+
+        Category.findAll().then(categories =>{
+            res.render("index", {articles: articles, categories: categories})
+        })
 
     })
 })
@@ -51,13 +54,37 @@ app.get("/:slug", (req, res)=>{
         }
     }).then(article =>{
         if(article != undefined){
-            res.render("article", {article: article})
+            Category.findAll().then(categories =>{
+                res.render("article", {article: article, categories: categories})
+            })
         }else{
             res.redirect("/")
         }
     }).catch(err =>{
         res.redirect("/")
 
+    })
+})
+
+app.get("/category/:slug", (req, res)=>{
+    let slug = req.params.slug
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model : Article}]
+    }).then(category =>{
+        if(category != undefined){
+
+            Category.findAll().then(categories =>{
+                res.render("index",{articles: category.articles, categories: categories})
+            })
+
+        }else{
+            res.redirect("/")
+        }
+    }).catch((err)=>{
+        res.redirect("/")
     })
 })
 
